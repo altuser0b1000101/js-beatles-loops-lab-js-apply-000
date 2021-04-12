55ed3da9782061c1c863b1cd7c5c4a144aeca2b8
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-12 06:53:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 function theBeatlesPlay(musicians,instruments){
 let arr = [];
 for ( let i=0; i < musicians.length; i++ ){
-  const str = musicians[i] + `plays` + instruments[i];
+  const str = `${musicians[i]} plays ${instruments[i]}`;
   arr.push(str);
   }
   return arr;
 }
-function johnLennonFacts(){
+function johnLennonFacts(facts){
+  
+  
+  function johnLennonFacts(facts) {
+  var array = [];
+  let i = 0;
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return array
+}
+  
+  
   const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
@@ -19,5 +32,32 @@ function johnLennonFacts(){
 
 
 
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) 
+  {
+    var string = `${musicians[i]} plays ${instruments[i]}`;
+    array.push(string);
+  }
+  return array
+}
 
+function johnLennonFacts(facts) {
+  var array = [];
+  let i = 0;
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return array
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  return array
+} 
 
