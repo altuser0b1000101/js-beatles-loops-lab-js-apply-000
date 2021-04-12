01bb4da9825f164931772b12cd2d3274a9474e77
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-12 06:39:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ let arr = [ ];
 } for ( let i=0; i < musicians.length; i++ ) {
   const str = musicians[ i ] + ' plays ' + instruments[ i ];
   arr.push( str );
+  return arr;
 }
-return arr;
+
+const whoPlaysWhat = theBeatlesPlay(
+  [ 'John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr' ],
+  [ 'Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums' ]
+);
+
+console.log( whoPlaysWhat );
