7ec1f2d0cd85ccecd547131a3d7582f8769d4ce8
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-12 06:50:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ for ( let i=0; i < musicians.length; i++ ){
   }
   return arr;
 }
-function johnLennonFacts(facts){
+function johnLennonFacts(){
   const facts = [
-    ]
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice",
+    ];
   
 }
 
