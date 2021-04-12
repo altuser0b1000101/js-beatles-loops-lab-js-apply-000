9da0fd9f420d521fc374ff1989e556eb710de84d
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-12 06:47:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function theBeatlesPlay(musicians,instruments){
 let arr = [];
-} for ( let i=0; i < musicians.length; i++ ) {
+for ( let i=0; i < musicians.length; i++ ){
   const str = musicians[i] + `"John Lennon plays guitar"` + instruments[i];
   arr.push(str);
   return arr;
+  }
 }
-
 function johnLennonFacts(){
   
 }
